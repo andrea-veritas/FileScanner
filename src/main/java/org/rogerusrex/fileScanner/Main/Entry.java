e529b8feecbfdbd3a56cb4e5372134d34813f641
roger.wang <roger.wang@tpadsz.com>
2016-03-31 00:21:46 +0800

    Added hashMethod option (md5 and sha1). Reformatted codes.

--- src/main/java/org/rogerusrex/fileScanner/Main/Entry.java
+++ src/main/java/org/rogerusrex/fileScanner/Main/Entry.java
@@ -11,19 +11,32 @@ import java.io.File;
  * Created by Roger on 2016/3/30.
  */
 public class Entry {
-    public static void main(String[] argv){
+    public static void main(String[] argv) {
 
-        Arguements args=new Arguements(argv);
+        Arguements args = new Arguements(argv);
 
-        File root=new File(args.getTargetDirectory());
-        if(root.exists()&&root.isDirectory()){
+        File root = new File(args.getTargetDirectory());
+        if (root.exists() && root.isDirectory()) {
 
         }
 
     }
 
-    public static class Arguements{
-        public Arguements(String[] argv){
+    public static class Arguements {
+        public final static String MODE_CSV = "mode.csv";
+        public final static String MODE_JSON = "mode.json";
+        public final static String HASH_MD5 = "hash.md5";
+        public final static String HASH_sha1 = "hash.sha1";
+        @Option(name = "-m", aliases = {"--mode"}, required = false)
+        private String outputMode = MODE_CSV;
+        @Option(name = "-o", required = false)
+        private String outputFile;
+        @Option(name = "-t", required = false)
+        private String targetDirectory = ".";
+        @Option(name = "-h", required = false)
+        private String hashMethod = HASH_MD5;
+
+        public Arguements(String[] argv) {
             CmdLineParser parser = new CmdLineParser(this);
             try {
                 parser.parseArgument(argv);
@@ -33,9 +46,6 @@ public class Entry {
             }
         }
 
-        public final static String MODE_CSV="mode.csv";
-        public final static String MODE_json="mode.json";
-
         public String getOutputMode() {
             return outputMode;
         }
@@ -43,15 +53,10 @@ public class Entry {
         public String getOutputFile() {
             return outputFile;
         }
+
         public String getTargetDirectory() {
             return targetDirectory;
         }
-        @Option(name = "-m", aliases = {"--mode"})
-        private String outputMode = MODE_CSV;
-        @Option(name="-o")
-        private String outputFile;
-        @Option(name="-t")
-        private String targetDirectory=".";
     }
 
 
